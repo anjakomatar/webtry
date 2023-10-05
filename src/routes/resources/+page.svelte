816293d9e3f20cc3d190c9e644f4b897f8d51a30
anjakomatar <mxanjakomatar@gmail.com>
2023-10-05 23:37:28 +0100

    New pages

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,12 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/introduction">introduction</a>
+  <a href="/services">services</a>
+  <a href="/resources">resources</a>
+  <a href="/community">community</a>
+  <a href="/community">FAQ</a>
+</nav>
+
+<header>
+  <h1>Resources</h1>
+</header>
