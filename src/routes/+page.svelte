2f83beb066c3c29b7c7702d8033a71ccb887055e
anjakomatar <mxanjakomatar@gmail.com>
2023-10-05 23:51:01 +0100

    New pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,3 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/introduction">introduction</a>
-  <a href="/services">services</a>
-  <a href="/resources">resources</a>
-  <a href="/community">community</a>
-  <a href="/FAQ">FAQ</a>
-</nav>
 
 <header>
   <h1>Maths and Statistics Space at University of Leeds</h1>
--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -1,11 +1,4 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/introduction">introduction</a>
-  <a href="/services">services</a>
-  <a href="/resources">resources</a>
-  <a href="/community">community</a>
-  <a href="/FAQ">FAQ</a>
-</nav>
+
 
 <header>
   <h1>FAQ</h1>
