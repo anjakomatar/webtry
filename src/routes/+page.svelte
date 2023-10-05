44be86f42eea27179a974a3a7ee63febe3977b14
anjakomatar <mxanjakomatar@gmail.com>
2023-10-05 21:37:58 +0100

    New content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+  // You can include any JS code if required here
+</script>
+
+<style>
+  /* Responsive styling */
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  nav {
+    background-color: #eee;
+    padding: 10px;
+  }
+
+  nav a {
+    margin-right: 20px;
+    text-decoration: none;
+    color: black;
+  }
+
+  img {
+    float: right;
+    max-width: 600px;
+    margin-left: 20px;
+  }
+</style>
+
+<nav>
+  <a href="#section1">Section 1</a>
+  <a href="#section2">Section 2</a>
+  <a href="#section3">Section 3</a>
+  <a href="#section4">Section 4</a>
+  <a href="#section5">Section 5</a>
+</nav>
+
+<h1>Maths and Statistics Space at University of Leeds</h1>
+
+<!-- Placeholder for the image -->
+<img src="path-to-your-image-of-edward-boyle-library.jpg" alt="Inside of Edward Boyle Library at the University of Leeds">
+
+<!-- Section 1 -->
+<h2 id="section1">Section 1: [Title Here]</h2>
+<p>Lorem ipsum text about the Maths and Statistics Space at the University of Leeds.</p>
+<p>More details...</p>
+
+<!-- Section 2 -->
+<h2 id="section2">Section 2: [Title Here]</h2>
+<p>Lorem ipsum text about the Maths and Statistics Space at the University of Leeds.</p>
+<p>More details...</p>
+
+<!-- Section 3 -->
+<h2 id="section3">Section 3: [Title Here]</h2>
+<p>Lorem ipsum text about the Maths and Statistics Space at the University of Leeds.</p>
+<p>More details...</p>
+
+<!-- Section 4 -->
+<h2 id="section4">Section 4: [Title Here]</h2>
+<p>Lorem ipsum text about the Maths and Statistics Space at the University of Leeds.</p>
+<p>More details...</p>
+
+<!-- Section 5 -->
+<h2 id="section5">Section 5: [Title Here]</h2>
+<p>Lorem ipsum text about the Maths and Statistics Space at the University of Leeds.</p>
+<p>More details...</p>
+
+<figcaption>Inside of Edward Boyle Library at the University of Leeds</figcaption>
